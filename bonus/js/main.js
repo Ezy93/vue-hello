a2722299e4f0774a83d35d48294b44c069aa8fea
Ezy93 <giordani001@gmail.com>
2022-03-16 14:36:52 +0100

    aggiunta immagine al dom tramite vuejs

---
+++ bonus/js/main.js
@@ -0,0 +1,15 @@
+/* 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data, magari volendo sperimentare anche prendendola da un input. */
+
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'hello Vue!',
+        image: 'https://i1.sndcdn.com/artworks-000142988650-icdihe-t500x500.jpg'
+    }
+})
